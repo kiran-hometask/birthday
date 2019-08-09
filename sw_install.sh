1a3d35485197ff2ccc174a095de576f332333bb7
kiran-hometask <53920032+kiran-hometask@users.noreply.github.com>
2019-08-09 11:09:10 +0100

    Update sw_install.sh

--- sw_install.sh
+++ sw_install.sh
@@ -5,4 +5,5 @@ ansible-galaxy install geerlingguy.docker
 export ANSIBLE_STDOUT_CALLBACK=debug
 ansible-playbook /home/ansible/birthday/dock_Jen_install.yml
 # Creating 3 containers as a service
+sudo docker swarm init
 sudo docker service create --name birthday-svc1 -p 90:8080 --replicas 3 chakkakiran/birthday
